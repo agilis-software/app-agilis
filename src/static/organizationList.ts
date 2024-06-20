641b2011b2de77a3b2bfff0274d547574a72bea1
smokeeaasd <lucasalencargomes111@gmail.com>
2024-06-20 01:37:49 -0300

    feat: create static lists

---
+++ src/static/organizationList.ts
@@ -0,0 +1,20 @@
+export const organizationList = [
+  {
+    id: 1,
+    title: 'Fatec Jahu',
+    description: 'Instituição de Ensino - Faculdade de Tecnologia',
+    createdAt: new Date('2024/03/20'),
+  },
+  {
+    id: 2,
+    title: 'Agilis Software',
+    description: 'Criadora do Agilis, software de gestão de tarefas',
+    createdAt: new Date('2024/03/30'),
+  },
+  {
+    id: 3,
+    title: 'Freelances',
+    description: 'Organização de projetos pessoais',
+    createdAt: new Date('2024/03/30'),
+  },
+]
