f3cb9beca2584a3d2d52cb51391283d0b89f062e
gabrielbotandev <gabrielficciobotan@gmail.com>
2024-10-29 22:54:48 -0300

    style: add gradient to login and signup background

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,7 +1,12 @@
 <template>
   <main
-    class=" sm:mx-auto w-full flex justify-center items-center flex-col min-h-screen bg-electric-violet-500"
+    class="relative sm:mx-auto w-full flex justify-center items-center flex-col min-h-screen bg-[#201E1E]"
   >
-    <RouterView />
+    <div
+      class="absolute inset-0 bg-gradient-to-br from-[#201E1E] to-[#8d00ec] opacity-70"
+    ></div>
+    <div class="relative z-10">
+      <RouterView />
+    </div>
   </main>
 </template>
