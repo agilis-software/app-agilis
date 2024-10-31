0bdc0f386dca6ddd166cb9a768756bc97f8c1757
gabrielbotandev <gabrielficciobotan@gmail.com>
2024-10-31 16:29:31 -0300

    refactor(input): use computed properties for type and icon

--- src/components/forms/InputPassword.vue
+++ src/components/forms/InputPassword.vue
@@ -1,37 +1,43 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-import { Icon } from '@iconify/vue';
-import eyeIcon from '@iconify-icons/mdi/eye';
-import eyeOffIcon from '@iconify-icons/mdi/eye-off';
+import { ref, computed } from 'vue'
+import { Icon } from '@iconify/vue'
+import eyeIcon from '@iconify-icons/mdi/eye'
+import eyeOffIcon from '@iconify-icons/mdi/eye-off'
 
 interface Props {
-  name: string;
-  value?: string;
-  validation?: string;
-  label?: string;
+  name: string
+  value?: string
+  validation?: string
+  label?: string
 }
 
-const props = defineProps<Props>();
-const showPassword = ref(false);
-const inputValue = ref('');
+const props = defineProps<Props>()
+const showPassword = ref(false)
+const inputValue = ref('')
+
+const inputType = computed(() => (showPassword.value ? 'text' : 'password'))
 
 const togglePasswordVisibility = () => {
-  showPassword.value = !showPassword.value;
-};
+  showPassword.value = !showPassword.value
+}
 
-const shouldShowIcon = () => inputValue.value.length > 0;
+const eyeIconType = computed(() => {
+  return showPassword.value ? eyeOffIcon : eyeIcon
+})
+
+const shouldShowIcon = () => inputValue.value.length > 0
 </script>
 
 <template>
-    <FormKit
-      v-bind="props"
-      :type="showPassword ? 'text' : 'password'"
-      v-model="inputValue" 
-    />
-    <Icon
-      v-if="shouldShowIcon()"
-      :icon="showPassword ? eyeOffIcon : eyeIcon"
-      @click="togglePasswordVisibility"
-      class="relative -inset-y-13 float-right right-3 cursor-pointer text-neutral-700 text-2xl"
-    />
+  <FormKit
+    v-bind="props"
+    :type="inputType"
+    v-model="inputValue"
+  />
+  <Icon
+    v-if="shouldShowIcon()"
+    :icon="eyeIconType"
+    @click="togglePasswordVisibility"
+    class="relative -inset-y-13 float-right right-3 cursor-pointer text-neutral-700 text-2xl"
+  />
 </template>
